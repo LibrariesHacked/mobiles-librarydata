16a4ac6025b4da11d935abbdd5d21bb97a899d96
Dave <dave_roweuk@yahoo.co.uk>
2017-02-05 19:59:33 +0000

    adding north somerset data

--- README.md
+++ README.md
@@ -9,6 +9,23 @@ Project to display mobile library timetables in an interactive live dashboard.
 
 
 
+## Mobile Library data format
+
+For this project to work, it needs to define a standard format to define the stop details of a mobile library.  Different authorities will have differing ways of describing this data.  
+
+| Column | Description | Example |
+| ------ | ----------- | ------- |
+| Route | A mobile library typically has a set of routes that it will follow.  This is an identifier for the route. | A |
+| Day | The day of the week that the route is driven. | Monday | 
+| StartDate | This is a reference date.  Mainly used to determine future dates that the route will occur on, based on the frequency. | 2017-01-09 |
+| Frequency | The frequency the route will be repeated.  Has to be a weekly identifier. |  |
+| Stop | The name of the stop. |  |
+| Place | The place the stop is located, normally a village or town name. |  |
+| Start | The time the mobile library arrives.  |  |
+| End | The time the mobile library leaves. |  |
+| X | The x coordinate of the library stop.  This could be longitude or easting. |  |
+| Y | The y coordinate of the library stop.  This could be latitude of northing. |  |
+
 ## Functionality
 
 
---
+++ scripts/northsomerset.py
@@ -0,0 +1,26 @@
+import csv
+from datetime import datetime
+
+mobiles = []
+with open('../data/northsomerset_raw.csv', 'r') as northsom_rawcsv:
+    mobreader = csv.reader(northsom_rawcsv, delimiter=',', quotechar='"')
+    next(mobreader, None)  # skip the headers
+    for row in mobreader:
+        community = row[0].strip()
+        stop = row[1].strip()
+        frequency = row[2].strip()
+        day = row[3].strip()
+        time = row[4].strip()
+        start = time.split('-')[0].strip()
+        end = time.split('-')[1].strip()
+        date = datetime.strptime(row[5].strip(), '%d %b %Y')
+        timetable = row[6].strip()
+        easting = row[7].strip()
+        northing = row[8].strip()
+        mobiles.append([community, stop, '', easting, northing, date.strftime('%Y-%m-%d'), day, frequency, start, end, timetable])
+
+with open('../data/northsomerset.csv', 'w', encoding='utf8', newline='') as northsomerset_csv:
+    mob_writer = csv.writer(northsomerset_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    mob_writer.writerow(['Route', 'Stop', 'Community', 'Address', 'Easting', 'Northing', 'Date', 'Day', 'Frequency', 'Start', 'End', 'Timetable'])
+    for stop in mobiles:
+        mob_writer.writerow([stop[0], stop[1], stop[2], stop[3], stop[4], stop[5], stop[6], stop[7], stop[8], stop[9], stop[10]])
