09716db255570eb7078e0b900b20623a75051a4d
Dave <dave_roweuk@yahoo.co.uk>
2017-03-16 21:21:41 +0000

    starting work on web interface

---
+++ js/maps.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    // Firstly load up the services
+    mobiles.loadServices(function (services) {
+        $.each(services, function (i, s) {
+            $('#sel-services').append($('<option>', {
+                value: s.code,
+                text: s.authority
+            }));
+        });
+    });
+});
---
+++ js/mobiles.js
@@ -0,0 +1,18 @@
+﻿var mobiles = {
+    services: [],
+    /////////////////////////////////////////////////
+    // Function: LoadServices
+    /////////////////////////////////////////////////
+    loadServices: function (callback) {
+        Papa.parse(config.servicesfile, {
+            download: true,
+            complete: function (results) {
+                this.services = results;
+                callback(services);
+            }
+        });
+    },
+    getMobileData: function (service, mobile) {
+
+    }
+};
