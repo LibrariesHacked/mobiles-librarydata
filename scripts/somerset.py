0b343eb0d40f85d5a593772a039aa57e06902842
Dave <dave_roweuk@yahoo.co.uk>
2017-09-18 22:31:32 +0100

    Somerset work

--- scripts/somerset.py
+++ scripts/somerset.py
@@ -2,6 +2,7 @@
 
 import csv
 import time
+import collections
 import urllib.request
 import json
 from datetime import datetime
@@ -12,6 +13,7 @@ DATA_OUTPUT = '../data/somerset.csv'
 def run():
     """Runs the main script"""
     mobiles = []
+    routes = collections.OrderedDict()
     with open(DATA_SOURCE, 'r') as som_raw:
         mobreader = csv.reader(som_raw, delimiter=',', quotechar='"')
         next(mobreader, None)
@@ -35,21 +37,28 @@ def run():
 
             easting = pc_data['result']['eastings']
             northing = pc_data['result']['northings']
+            latitude = pc_data['result']['latitude']
+            longitude = pc_data['result']['longitude']
 
-            mobiles.append(
-                [route, stop, community, address, easting, northing, date.strftime('%Y-%m-%d'),
-                 day, frequency, start, end, timetable])
+            mobile = [route, stop, community, address, easting, northing, longitude,
+                      latitude, date.strftime('%Y-%m-%d'), day, frequency, start, end, timetable]
+
+            mobiles.append(mobile)
 
             time.sleep(1)
+
+            if routes.get(route) is None:
+                routes[route] = collections.OrderedDict()
+            routes[route][stop] = [latitude, longitude]
 
     with open(DATA_OUTPUT, 'w', encoding='utf8', newline='') as out_csv:
         mob_writer = csv.writer(out_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         mob_writer.writerow(
-            ['Route', 'Stop', 'Community', 'Address', 'Easting', 'Northing',
-             'Date', 'Day', 'Frequency', 'Start', 'End', 'Timetable'])
+            ['Route', 'Stop', 'Community', 'Address', 'Easting', 'Northing', 'Longitude',
+             'Latitude', 'Date', 'Day', 'Frequency', 'Start', 'End', 'Timetable'])
         for stop in mobiles:
             mob_writer.writerow(
-                [stop[0], stop[1], stop[2], stop[3], stop[4],
-                 stop[5], stop[6], stop[7], stop[8], stop[9], stop[10], stop[11]])
+                [stop[0], stop[1], stop[2], stop[3], stop[4], stop[5], stop[6],
+                 stop[7], stop[8], stop[9], stop[10], stop[11], stop[12], stop[13]])
 
 run()
