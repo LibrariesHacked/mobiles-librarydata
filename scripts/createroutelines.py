dbfe0f2acfb3caf1c4bc00e9b8417b4e6dde4744
Dave <dave_roweuk@yahoo.co.uk>
2017-10-06 21:38:09 +0100

    working on route script

--- scripts/createroutelines.py
+++ scripts/createroutelines.py
@@ -18,26 +18,34 @@ def run():
             if data != "":
                 authorities.append({ 'code':code, 'filename':data, 'authority':authority })
                 
-    for a in authorities:
-        with open('../data/' + a['filename'], 'rb') as mobilecsv:
+    for auth in authorities:
+        with open('../data/' + auth['filename'], 'rb') as mobilecsv:
             reader = csv.reader(mobilecsv, delimiter=',', quotechar='"')
             next(reader, None)  # skip the headers
             routes = collections.OrderedDict()
-            for row in reader:
-                library = row[1]
-                route = row[2]
-                routeId = library + route
-                if routes.get(routeId) is None:
-                    routes[routeId] = collections.OrderedDict()
-                latitude = row[11]
-                longitude = row[12]
-                id = row[0]
-                routes[routeId][id] = [latitude,longitude]
+            for idx, row in reader:
+                route = row[0]
+                stop = row[1]
+                community = row[2]
+                address = row[3]
+                easting = row[4]
+                northing = row[5]
+                date = row[6]
+                day = row[7]
+                frequency = row[8]
+                start = row[9]
+                end = row[10]
+                timetable = row[11]
+                if routes.get(route) is None:
+                    routes[route][idx] = collections.OrderedDict()
+                point = geopandas.GeoSeries([Point(easting, northing)])
+                point.crs = {'init' :'epsg:27700'}
+                point = world.to_crs({'init': 'epsg:4326'}) 
+                routes[route][idx] = [point.x,point.y]
 
         url = ''
         for route in routes:
-            for idx,trip in enumerate(routes[route]):
-        
+            for idx,trip in enumerate(routes[route]):        
                 if idx == 0:
                     waypoints = ''
                     url = 'http://openls.geog.uni-heidelberg.de/route?start=' + routes[route][trip][1] + ',' + routes[route][trip][0]
@@ -51,5 +59,4 @@ def run():
                 else:
             waypoints = waypoints + ' ' + routes[route][trip][1] + ',' + routes[route][trip][0]
 
-        
 run()
