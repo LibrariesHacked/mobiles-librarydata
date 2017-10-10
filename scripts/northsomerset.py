40e83d79117e5ececa59c4655be2a72bc709ff4f
Dave <DaveBathnes@users.noreply.github.com>
2017-10-10 14:23:21 +0100

    Correcting errors in script

--- scripts/northsomerset.py
+++ scripts/northsomerset.py
@@ -2,7 +2,6 @@
 
 import csv
 from datetime import datetime
-from string import ascii_uppercase
 
 DATA_SOURCE = '../data/northsomerset_raw.csv'
 DATA_OUTPUT = '../data/northsomerset.csv'
@@ -11,7 +10,6 @@ def run():
     """Runs the main script"""
     mobiles = []
     routes = {}
-    char_index = 0
 
     with open(DATA_SOURCE, 'r') as northsom_raw:
         mobreader = csv.reader(northsom_raw, delimiter=',', quotechar='"')
@@ -32,13 +30,6 @@ def run():
             address = stop + ', ' + community
             date_output = date.strftime('%Y-%m-%d')
 
-            # We don't have a route number but need a way of grouping together stops
-            if (day + ' ' + date_output) not in routes:
-                routes[day + ' ' + date_output] = ascii_uppercase[char_index]
-                char_index = char_index + 1
-
-            route = routes[day + ' ' + date_output]
-
             mobiles.append(
                 [mobile, route, community, stop, address, easting, northing, date_output,
                  day, frequency, start, end, timetable])
@@ -51,5 +42,5 @@ def run():
         for sto in mobiles:
             mob_writer.writerow(
                 [sto[0], sto[1], sto[2], sto[3], sto[4], sto[5],
-                 sto[6], sto[7], sto[8], sto[9], sto[10], sto[11]])     
+                 sto[6], sto[7], sto[8], sto[9], sto[10], sto[11], sto[12]])     
 run()
