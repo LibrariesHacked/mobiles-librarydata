103a43a0b86314354589ec17d75df4333c9cf7cf
Dave <dave_roweuk@yahoo.co.uk>
2017-11-05 20:01:10 +0000

    working on wiltshire

--- scripts/wiltshire.py
+++ scripts/wiltshire.py
@@ -10,7 +10,10 @@ import pyproj
 from bs4 import BeautifulSoup
 
 APIKEY = '58d904a497c67e00015b45fcc3d8ff500a48497b5dd7a68a81baf0b2'
-DATA_OUTPUT = '../data/wiltshire.csv'
+DATA_OUTPUT_RAW = '../data/raw/wiltshire_raw.csv'
+DATA_OUTPUT_FINAL = '../data/wiltshire.csv'
+# Wiltshire County bounding box coordinates for geocoding
+BBOX = '-2.3656,50.945,-1.4857,51.7031'
 
 def run():
     """Runs the main script"""
@@ -19,33 +22,75 @@ def run():
     stop_list = 'http://services.wiltshire.gov.uk/MobileLibrary/Library/StopList'
     stop_list_html = requests.get(stop_list)
     stop_list_soup = BeautifulSoup(stop_list_html.text, 'html.parser')
+    
+    # If we don't already have it, create the raw file
+    if not os.path.isfile(DATA_OUTPUT_RAW):
 
-    mobiles = []
-	# For each stop get the stop details
-    for link in stop_list_soup.find_all('a'):
-        # Detect wether the link is a link to a stop
-        if '/MobileLibrary/Library/Stop' in link.get('href'):
+      mobiles = []
+      # For each stop get the stop details
+      for link in stop_list_soup.find_all('a'):
+          # Detect whether the link is a link to a stop
+          if '/MobileLibrary/Library/Stop' in link.get('href'):
+  
+              # Get the webpage
+              stop_url = 'http://services.wiltshire.gov.uk' + link.get('href')
+              stop_html = requests.get(stop_url)
+              stop_soup = BeautifulSoup(stop_html.text, 'html.parser')
+  
+              # General stop information
+              stop_name = stop_soup.find('h2').text
+              community = stop_name.split(', ')[0]
+              stop_name = stop_name.split(', ')[1]
+              address = stop_name + ', ' + community
+              # There are some stops that are two weekly but they're part of separate routes.  Keep them separate
+              frequency = 4
+  
+              # Detailed information for the stop is found in the table.
+              table = stop_soup.find('table').find('tbody')
+              stop_rows = table.find_all('tr')
+  
+              for stop in stop_rows:
+                  round_name = stop.find('a').text.replace('\r\n', '').replace(' (fortnightly stop)', '')
+                  mobile_library = round_name.split(', ')[0].replace(' mobile library', '')
+                  day_week = round_name.split(', ')[1]
+                  route = day_week.replace('week', 'Week')
+                  week = day_week.split(' week ')[1]
+                  day = day_week.split(' week ')[0]
+                  date = datetime.strptime(stop.find('li').text, '%A %d %B, %Y')
+                  date_output = date.strftime('%Y-%m-%d')
+                  start = stop.find_all('td')[1].text
+                  end = stop.find_all('td')[2].text
+                  timetable = 'http://services.wiltshire.gov.uk' + stop.find('a').get('href')
+  
+                  # Mobile,Route,Stop,Community,Address,Longitude,Latitude,Date,Day,Frequency,Start,End,Timetable
+                  mobile = { 'mobile': mobile_library, 'route': route,
+                      'stop': stop_name, 'community': community, 'address': address, 'longitude': longitude,
+                      'latitude': latitude, 'date': date_output, 'day': day, 'frequency': frequency, 'start': start, 'end': end, 'timetable': timetable }
+                  mobiles.append(mobile)
+  
+                  print(mobile)
 
-            # Get the webpage
-            stop_url = 'http://services.wiltshire.gov.uk' + link.get('href')
-            print(stop_url)
-            stop_html = requests.get(stop_url)
-            stop_soup = BeautifulSoup(stop_html.text, 'html.parser')
+      with open(DATA_OUTPUT_RAW, 'w', encoding='utf8', newline='') as out_raw:
+          mob_writer = csv.writer(out_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+          mob_writer.writerow(
+              ['Mobile', 'Route', 'Community', 'Stop', 'Address', 'Longitude',
+               'Latitude', 'Date', 'Day', 'Frequency', 'Start', 'End', 'Timetable'])
+          for sto in mobiles:
+              mob_writer.writerow(
+                  [
+                      sto['mobile'], sto['route'], sto['community'], sto['stop'], sto['address'], sto['date'],
+                      sto['day'], sto['frequency'], sto['start'], sto['end'], sto['timetable']
+                  ])
 
-            # General stop information
-            stop_name = stop_soup.find('h2').text
-            community = stop_name.split(', ')[0]
-            stop_name = stop_name.split(', ')[1]
-            address = stop_name + ', ' + community
-            frequency = 4
-
-            # Wiltshire County bounding box coordinates for geocoding
-            boundingbox = '-2.3656,50.945,-1.4857,51.7031'
-
+    with open(DATA_OUTPUT_RAW, 'r', encoding='utf8', newline='') as raw:
+        mobreader = csv.reader(raw, delimiter=',', quotechar='"')
+        next(mobreader, None)  # skip the headers
+        # Mobile,Route,Community,Stop,Address,Date,Day,Frequency,Start,End,Timetable
+        for row in mobreader:
             # Initial geocoding URL - use just the community to try to get the right area
             geo_url = (
                 'https://api.openrouteservice.org/geocoding?boundary_type=rect&rect=' + boundingbox +
-                '&api_key=' + APIKEY + '&query=' + quote(community) + '&limit=1')
+                '&api_key=' + APIKEY + '&query=' + quote(row[2]) + '&limit=1')
             print(geo_url)
 
             attempts = 0
@@ -68,7 +113,7 @@ def run():
             geo_url = (
                 'https://api.openrouteservice.org/geocoding?boundary_type=rect&rect=' +
                 boundingbox + '&api_key=' + APIKEY + '&query=' +
-                quote(address) + '&lang=en&limit=1')
+                quote(row[4]) + '&lang=en&limit=1')
             print(geo_url)
 
             attempts = 0
@@ -92,41 +137,19 @@ def run():
                     longitude = lng
                     latitude = lat
 
-            # Detailed information for the stop is found in the table.
-            table = stop_soup.find('table').find('tbody')
-            stop_rows = table.find_all('tr')
-
-            for stop in stop_rows:
-                round_name = stop.find('a').text.replace('\r\n', '').replace(' (fortnightly stop)', '')
-                mobile_library = round_name.split(', ')[0].replace(' mobile library', '')
-                day_week = round_name.split(', ')[1]
-                route = day_week.replace('week', 'Week')
-                week = day_week.split(' week ')[1]
-                day = day_week.split(' week ')[0]
-                date = datetime.strptime(stop.find('li').text, '%A %d %B, %Y')
-                date_output = date.strftime('%Y-%m-%d')
-                start = stop.find_all('td')[1].text
-                end = stop.find_all('td')[2].text
-                timetable = 'http://services.wiltshire.gov.uk' + stop.find('a').get('href')
-
-                # Mobile,Route,Stop,Community,Address,Longitude,Latitude,Date,Day,Frequency,Start,End,Timetable
-                mobile = { 'mobile': mobile_library, 'route': route,
-                    'stop': stop_name, 'community': community, 'address': address, 'longitude': longitude,
-                    'latitude': latitude, 'date': date_output, 'day': day, 'frequency': frequency, 'start': start, 'end': end, 'timetable': timetable }
-                mobiles.append(mobile)
-
-                print(mobile)
-
-    with open(DATA_OUTPUT, 'w', encoding='utf8', newline='') as out_csv:
-        mob_writer = csv.writer(out_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        mob_writer.writerow(
-            ['Mobile', 'Route', 'Community', 'Stop', 'Address', 'Longitude',
-             'Latitude', 'Date', 'Day', 'Frequency', 'Start', 'End', 'Timetable'])
-        for sto in mobiles:
-            mob_writer.writerow(
-                [
-                    sto['mobile'], sto['route'], sto['community'], sto['stop'], sto['address'], sto['longitude'],
-                    sto['latitude'], sto['date'], sto['day'], sto['frequency'], sto['start'], sto['end'], sto['timetable']
-                ])
+      with open(DATA_OUTPUT_RAW, 'w', encoding='utf8', newline='') as out_raw:
+          mob_writer = csv.writer(out_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+          mob_writer.writerow(
+              ['Mobile', 'Route', 'Community', 'Stop', 'Address', 'Longitude',
+               'Latitude', 'Date', 'Day', 'Frequency', 'Start', 'End', 'Timetable'])
+          for sto in mobiles:
+              mob_writer.writerow(
+                  [
+                      row[0], row[1], row[2], row[3], row[4], sto['date'],
+                      sto['day'], sto['frequency'], sto['start'], sto['end'], sto['timetable']
+                  ])
+                
+                
+        
 
 run()
